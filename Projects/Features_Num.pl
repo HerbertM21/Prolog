ed954591d8cec09616e5d27c32869bf04b7bad78
Herbert Mayorga <87925098+HerbertM21@users.noreply.github.com>
2022-11-14 00:59:47 -0300

    Update and rename CaracteristicasNum.pl to Features_Num.pl

--- Projects/Features_Num.pl
+++ Projects/Features_Num.pl
@@ -1,9 +1,6 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % BASE DE CONOCIMIENTO CARACTERISTICAS DE NUMERO %
 % - HERBERT MAYORGA                              %
-% - GABRIEL LEIVA                                %
-% - BENJAMIN POBLETE                             %
-% - JEAN LOZANO                                  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -27,12 +24,12 @@ cantidad(X,N):- X > 0, X2 is X//10, cantidad(X2,N2), N is N2+1.
 
 % ========== VERIFICACION NUM =============
 % >> NUMERO PAR
-esPar(X):- X > 0, verificarPar(X, 2), !; write('¡ERROR! NUMERO NEGATIVO').
+esPar(X):- X > 0, verificarPar(X, 2), !; write('Â¡ERROR! NUMERO NEGATIVO').
 
 verificarPar(X, Y):- 0 is X mod Y, write('El numero es par.'); write('El numero no es par.').
 
 % >> NUMERO PRIMO
-esPrimo(X):- X > 0, verificarPrimo(X,2), !; write('¡ERROR! NUMERO NEGATIVO').
+esPrimo(X):- X > 0, verificarPrimo(X,2), !; write('Â¡ERROR! NUMERO NEGATIVO').
 
 verificarPrimo(X, Y):- Y >= X, write('El numero es primo.');
 0 is X mod Y, write('El numero no es primo.');Y2 is Y+1,
@@ -48,7 +45,7 @@ verificarPrimo(X, Y):- Y >= X, write('El numero es primo.');
 % K = LONGITUD DE DIGITOS DE X
 
 esNarciso(X):- cantidad(X, K), X > 0, N is X mod 10, Y is N^K, Z is X//10,
-    verificarNarciso(Z, X, Y, K), !; write('¡ERROR! NÚMERO NEGATIVO').
+    verificarNarciso(Z, X, Y, K), !; write('Â¡ERROR! NÃšMERO NEGATIVO').
 
 verificarNarciso(Z, X, Y, _):- Z = 0, X == Y, write('El numero es narciso.'), !.
 verificarNarciso(Z, X, Y, _):- Z = 0, X \= Y, write('El numero no es narciso.').
@@ -57,7 +54,7 @@ verificarNarciso(Z, X, Y, K):- Z > 0, T is Z mod 10, C is T^K,
 
 % >> RAIZ CUADRADA EXACTA
 esRaizExacta(X):- X > 0, Y is integer(sqrt(X))*integer(sqrt(X)),
-    verificarRaiz(X,Y),!; write('¡ERROR! NÚMERO NEGATIVO').
+    verificarRaiz(X,Y),!; write('Â¡ERROR! NÃšMERO NEGATIVO').
 
 verificarRaiz(X,Y):- X == Y -> write('El numero tiene raiz cuadrada exacta.');
 write('El numero no tiene raiz cuadrada exacta.').
